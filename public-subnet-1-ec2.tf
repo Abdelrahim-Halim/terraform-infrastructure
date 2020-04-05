fa7ec8890d40eeb2b31ac9909b3e072e471f157f
ahmed emad <ahmed.emad2051@gmail.com>
2020-04-05 20:04:45 +0200

    fix key

--- public-subnet-1-ec2.tf
+++ public-subnet-1-ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "nginx-ec2-1" {
   ami           = "ami-0fc61db8544a617ed"
   instance_type = "t2.micro"
-  key_name = "${aws_key_pair.deployer.key_name}"
+  key_name = "${aws_key_pair.deployer-new.key_name}"
   subnet_id = "${aws_subnet.pub1.id}"
   associate_public_ip_address = true
   security_groups = ["${aws_security_group.http-traffic.id}"]
@@ -13,7 +13,7 @@ resource "aws_instance" "nginx-ec2-1" {
 resource "aws_instance" "bastion" {
   ami           = "ami-0fc61db8544a617ed"
   instance_type = "t2.micro"
-  key_name = "${aws_key_pair.deployer.key_name}"
+  key_name = "${aws_key_pair.deployer-new.key_name}"
   subnet_id = "${aws_subnet.pub1.id}"
   associate_public_ip_address = true
   security_groups = ["${aws_security_group.ssh-traffic.id}"]
--- public-subnet-2-ec2.tf
+++ public-subnet-2-ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "nginx-ec2-2" {
   ami           = "ami-0fc61db8544a617ed"
   instance_type = "t2.micro"
-  key_name = "${aws_key_pair.deployer.key_name}"
+  key_name = "${aws_key_pair.deployer-new.key_name}"
   subnet_id = "${aws_subnet.pub2.id}"
   associate_public_ip_address = true
   security_groups = ["${aws_security_group.http-traffic.id}"]
