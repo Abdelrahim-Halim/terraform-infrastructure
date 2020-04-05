fdfc4693c56059c4d0bd19beec4679b39bdaeef8
ahmed emad <ahmed.emad2051@gmail.com>
2020-04-05 20:00:02 +0200

    fix key

--- key.tf
+++ key.tf
@@ -1,23 +1,23 @@
-resource "tls_private_key" "ssh-keygen" {
+resource "tls_private_key" "ssh-keygen-new" {
   algorithm   = "RSA"
   ecdsa_curve = "2048"
 }
 
 resource "aws_key_pair" "deployer" {
   key_name   = "deployer-key"
-  public_key = "${tls_private_key.ssh-keygen.public_key_openssh}"
+  public_key = "${tls_private_key.ssh-keygen-new.public_key_openssh}"
 }
 
-resource "aws_secretsmanager_secret" "ssh-secret-private-key" {
-  name = "ssh-secret-key"
+resource "aws_secretsmanager_secret" "ssh-secret-private-key-new" {
+  name = "ssh-secret-key-new"
 }
 
-resource "aws_secretsmanager_secret_version" "ec2-ssh-private-key" {
-  secret_id     = "${aws_secretsmanager_secret.ssh-secret-private-key.id}"
-  secret_string = "${tls_private_key.ssh-keygen.private_key_pem}"
+resource "aws_secretsmanager_secret_version" "ec2-ssh-private-key-new" {
+  secret_id     = "${aws_secretsmanager_secret.ssh-secret-private-key-new.id}"
+  secret_string = "${tls_private_key.ssh-keygen-new.private_key_pem}"
 }
 
-resource "local_file" "ec2-private-key" {
-    content     = "${tls_private_key.ssh-keygen.private_key_pem}"
+resource "local_file" "ec2-private-key-new" {
+    content     = "${tls_private_key.ssh-keygen-new.private_key_pem}"
     filename = "${path.module}/ec2.pem"
 }
